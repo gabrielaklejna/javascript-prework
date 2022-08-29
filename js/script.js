4f9ac57cb28014db0513bdab1c9d0d850e569314
Gabriela Klejna <gabriela.rentel@gmail.com>
2022-08-29 17:34:54 +0200

    add .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-function playGame(1,2,3){
-  playerInput();
-  clearMessages();
- function getMoveName(moveNumber){
+function playGame(playerInput){
+  
+  function getMoveName(moveNumber){
   if(moveNumber == 1){
     return 'kamień';
   } else if(moveNumber == 2){
@@ -12,40 +11,57 @@ function playGame(1,2,3){
     printMessage('Nie znam ruchu o id ' + moveNumber + '.');
     return 'nieznany ruch';
   }
- }
+  }
+
+  /*
+   function getMoveName(moveNumber){
+    ...
+  } 
+  ->
+  const getMoveName = function(moveNumber){\
+    ...
+  }
+  
+  */
+
+
  function displayResult(argComputerMove, argPlayerMove){
   console.log('moves:', argComputerMove, argPlayerMove);
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-  if( argComputerMove == 'kamień' && argPlayerMove == 'papier')||
+  if((argComputerMove == 'kamień' && argPlayerMove == 'papier')||
     ( argComputerMove == 'papier' && argPlayerMove == 'nożyce')||
-    ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
       printMessage('Ty wygrywasz!');
   } else if (argComputerMove == argPlayerMove){
     printMessage('Remis')
   } else {
     printMessage('Tym razem przegrywasz :(');
   }
-}
- let randomNumber = Math.floor(Math.random() * 3 + 1); //1 | 2 | 3
- let computerMove = getMoveName(randomNumer); //"kamień" | "papier" | "nożyce"
- printMessage('Mój ruch to: ' + computerMove);
+  }
 
- //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
- let playerMove = getMoveName(playerInput);
+  clearMessages();
 
- displayResult(playerMove, computerMove);
-}
-function buttonClicked(){
-  printMessage('zagrałeś' + playerMove);
+  const randomNumber = Math.floor(Math.random() * 3 + 1); //1 | 2 | 3
+  const computerMove = getMoveName(randomNumber); //"kamień" | "papier" | "nożyce"
+  printMessage('Mój ruch to: ' + computerMove);
+
+  const playerMove = getMoveName(playerInput);
+
+  displayResult(playerMove, computerMove);
 }
 
-let playRock = document.getElementById('kamień');
-playRock.addEventListener('click', buttonClicked);
+const playRock = document.getElementById('playRock');
+playRock.addEventListener('click', function() {
+  playGame(1);
+});
 
-let playPaper = document.getElementById('papier');
-playPaper.addEventListener('click', buttonClicked);
+const playPaper = document.getElementById('playPaper');
+playPaper.addEventListener('click', function() {
+  playGame(2);
+});
 
-let playScrissors = document.getElementById('nożyce');
-playRock.addEventListener('click', buttonClicked);
+const playScrissors = document.getElementById('playScrissors');
+playScrissors.addEventListener('click', function() {
+  playGame(3);
+});
